f468c6c7558ab808b686c1dec06976c6a17cc778
kevinchen6666 <126632161+kevinchen6666@users.noreply.github.com>
2024-10-02 22:55:47 -0700

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -2,14 +2,26 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+// Import the shared user store from index.js
+const users = require('./index').users;
+
 router.get('/signup', (req, res) => {
   res.sendFile(path.join(__dirname, '/pages/signup.html'));
 });
 
 router.post('/signup', (req, res) => {
   const { username, password } = req.body;
-  // Sign-up logic here (store user credentials in a real database)
-  res.send('Sign-up successful. Now you can login.');
+
+  // Check if the username is already taken
+  const existingUser = users.find(user => user.username === username);
+  
+  if (existingUser) {
+    res.send('Username already exists. Please choose another one.');
+  } else {
+    // Add new user to the in-memory store
+    users.push({ username, password });
+    res.send('Sign-up successful. Now you can login.');
+  }
 });
 
 module.exports = router;

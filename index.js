bd22679de6cb8be2095bdb82725c58351b886abb
kevinchen6666 <126632161+kevinchen6666@users.noreply.github.com>
2024-10-02 23:13:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const axios = require('axios');
-const app = express();
 const path = require('path');
+const axios = require('axios');
+
+const app = express();
 
 // In-memory user store
 let users = [];
@@ -10,9 +11,45 @@ let users = [];
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve static files (HTML, CSS) from the "pages" and "styles" directory
+// Serve static files (HTML, CSS) from the "pages" and "styles" directories
 app.use(express.static(path.join(__dirname, 'pages')));
 app.use(express.static(path.join(__dirname, 'styles')));
+
+// Root route - serve the login page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/login.html'));
+});
+
+// Login POST route
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(user => user.username === username && user.password === password);
+
+  if (user) {
+    res.redirect('/webhook');
+  } else {
+    res.send('Invalid credentials. Please try again.');
+  }
+});
+
+// Signup page route
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/pages/signup.html'));
+});
+
+// Signup POST route
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  const existingUser = users.find(user => user.username === username);
+
+  if (existingUser) {
+    res.send('Username already exists. Please choose another one.');
+  } else {
+    users.push({ username, password });
+    res.send('Sign-up successful. Now you can login.');
+  }
+});
 
 // Webhook page route
 app.get('/webhook', (req, res) => {
@@ -40,8 +77,9 @@ app.post('/send-webhook', (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
